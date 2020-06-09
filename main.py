c14dd4ea33fdbcd43aa8f322858a1429715b27dd
Ana.Guerrero <ana.analista93@gmail.com>
2020-06-09 05:03:17 -0400

    retomando curso, se agrega welcome para repasar lo aprendido

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     user_ip = request.remote_addr
-    response = make_response(redirect('/home'))
+    ip_usuario = request.remote_addr
+    response = make_response(redirect('/welcome'))
     response.set_cookie('user_ip', user_ip)
+    response.set_cookie('usuario_ip', ip_usuario)
+
     return response
 
 
@@ -14,3 +17,10 @@ def index():
 def hello():
     user_ip = request.cookies.get('user_ip')
     return render_template('hello.html', user_ip=user_ip)
+
+@app.route('/welcome')
+def welcome():
+    ip_usuario = request.cookies.get('usuario_ip')
+
+    return render_template('welcome.html', usuario_ip=ip_usuario)
+    #return 'Hola hola maquinola, tu ip es {}'.format(ip_usuario)
